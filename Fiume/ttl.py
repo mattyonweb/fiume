7913feb9576272f940ad8fe53542d4866f572e49
Matteo Cavada <cvd00@insicuri.net>
2021-07-05 13:55:06 +0200

    Implemented TTL table with tests

--- Fiume/ttl.py
+++ Fiume/ttl.py
@@ -1,64 +1,100 @@
 from typing import *
-from queue import Queue
+from queue import Queue, Empty
 import time
+import threading
 
+Timestamp = int
+TTL = float
+
+class AlreadyPresent(KeyError):
+    pass
+    
 class TTL_table:
-    def __init__(self):
-        self.ttl: Dict[Hashable, int] = dict()
+    def __init__(self, default_ttl: float):
+        self.default_ttl = default_ttl
+
+        # Objs che sono stati aggiunti ma non ancora estratti
+        self.not_yet_extracted: Dict[Hashable, Tuple[TTL, threading.Thread]] = dict()
+        # Objs già estratti ma ancora in fase di expiration
+        self.recently_extracted: Dict[Hashable, Tuple[TTL, threading.Thread]] = dict()
+        # Objs estratti
         self.available: Queue = Queue()
 
-    def add(self, obj: Hashable, ttl: int, starting_time: int=None):
-        """ 
-        Adds an object to the TTL table
-        """
-        if starting_time is None:
-            starting_time = int(time.time())
-
-        self.ttl[obj] = starting_time + ttl
+        self._lock = threading.Lock()
+        self._cond = threading.Condition(self._lock)
+        self._not_empty = threading.Condition(self._lock)
 
         
-    def _update_available(self) -> bool:
+    def add(self, obj: Hashable) -> int:
+        """ 
+        Adds an object to the TTL table. 
+
+        Returns the TTL for the added object.
         """
-        Private method. Checks for timeouts in the table.
-        """
-        current_time = int(time.time())
+        with self._lock:
+            if obj in self.not_yet_extracted:
+                raise AlreadyPresent()
 
-        to_delete = list()
-        for obj, expiration in self.ttl.items():
-            if expiration <= current_time:
-                self.available.put(obj)
-                to_delete.append(obj)
-
-        for obj in to_delete:
-            del self.ttl[obj]
-
+            ttl = self.default_ttl
             
+            if obj in self.recently_extracted:
+                ttl = self.recently_extracted[obj][0] * 2
+
+                # deactive timer thread for recently_extracted auto-destruction
+                self.recently_extracted[obj][1].cancel()
+                del self.recently_extracted[obj]
+
+                
+            t = threading.Timer(ttl, lambda: self.available.put(obj))    
+            self.not_yet_extracted[obj] = (ttl, t)
+            t.start()
+
+        return ttl
+
+    
     def any_ready(self) -> bool:
         """
         Returns whether there is any available (aka. expired) object.
         """
-        self._update_available()
         return not self.available.empty()
 
     
-    def extract(self, n=1, blocking=True, timeout=None) -> List[Any]:
+    def extract(self, n=1, timeout=None, accontentati=False) -> List[Any]:
         """
         Extracts n objects from the expired set. 
         
-        If blocking is True, waits (perhaps for `timeout` seconds) for 
-        the queue to be filled up.
+        Timeout is the timeout for an object when our queue is empty. None means forever.
+
+        Accontentati is used when you request the extraction of `n` objects, 
+        but only `m` (`m` < `n`) are inside the queue. If Accontentati is True,
+        you return only `m` objects; otherwise, wait for `n` objects.
         """
-        self._update_available()
         out = list()
         
-        if blocking:
-            for _ in range(n):
-                out.append(self.available.get(timeout=1))
-        else:
-            for _ in range(n):
-                if self.available.empty():
+        for _ in range(n):
+            try:
+                obj = self.available.get() if timeout is None else self.available.get(timeout=timeout)
+                out.append(obj)
+            except Empty as e:
+                if accontentati:
                     return out
-                out.append(self.available.get())
+                raise e
+
+            # Add entry in recently_extracted
+            self.recently_extracted[obj] = (
+                self.not_yet_extracted[obj][0],
+                threading.Timer(self.not_yet_extracted[obj][0],
+                                lambda: self._del(self.recently_extracted, obj))
+            )
+
+            self.recently_extracted[obj][1].start()
+            self._del(self.not_yet_extracted, obj)
 
         return out
+    
 
+    def _del(self, d, key):
+        if key in d:
+            del d[key]
+        else:
+            pass # maybe key was already extracted from d in other ways
--- tests/test_ttl.py
+++ tests/test_ttl.py
@@ -8,40 +8,54 @@ import random
 import tempfile
 import time
 
-from Fiume.ttl import TTL_table
+# from Fiume.ttl import TTL_table
+from Fiume.ttl_cond import TTL_table
 
 class TTL_tests(unittest.TestCase):
     def setUp(self):
-        self.ttl = TTL_table()
+        self.ttl = TTL_table(1)
 
     def test_empty_ttl_has_nothing_ready(self):
         self.assertTrue(not self.ttl.any_ready())
 
     def test_simple_blocking_add(self):
-        self.ttl.add("a", 0)
-        self.assertTrue(self.ttl.any_ready())
-        self.assertTrue(self.ttl.extract(n=1, blocking=True) == ["a"])
-        self.assertTrue(not self.ttl.any_ready())
-
-    def test_simple_blocking_add(self):
-        self.ttl.add("a", 1)
+        self.ttl.add("a")
+        
         self.assertTrue(not self.ttl.any_ready())
         time.sleep(1)
-        self.assertTrue(self.ttl.extract(n=1, blocking=True) == ["a"])
+        self.assertTrue(self.ttl.extract(n=1) == ["a"])
 
     def test_blocking_timeout(self):
-        self.ttl.add("a", 10)
+        self.ttl.add("a")
         try:
-            self.ttl.extract(n=2, blocking=True)
+            self.ttl.extract(n=2, timeout=1)
             self.assertTrue(False, "Should have raised Empty!")
         except Empty:
             pass
 
-    def test_non_blocking(self):
-        self.ttl.add("a", 0)
-        self.ttl.add("b", 0.5)
-        self.ttl.add("c", 100)
-
-        time.sleep(1)
-        self.assertEqual(self.ttl.extract(n=3, blocking=False), ["a", "b"])
+    def test_blocking_timeout(self):
+        self.ttl.add("a")
+        try:
+            l = self.ttl.extract(n=1, timeout=0.5, accontentati=True)
+            self.assertEqual(l, [])
+        except Empty:
+            self.assertTrue(False, "Should have returned empty list!")
         
+    def test_non_blocking(self):
+        self.ttl.add("a")
+        self.ttl.add("b")
+        time.sleep(0.5)
+        self.ttl.add("c")
+
+        time.sleep(0.5)
+        self.assertEqual(
+            self.ttl.extract(n=3, timeout=0, accontentati=True),
+            ["a", "b"]
+        )
+        
+    def test_exponential_backoff(self):
+        self.ttl.add("A")
+        time.sleep(1)
+        self.ttl.extract(n=1)
+        
+        self.assertEqual(self.ttl.add("A"), 2)
